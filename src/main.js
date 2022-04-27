e422d3a2729ec15f50c11b1a5936b8988f060b12
ECarry <lianshiliang93@gmail.com>
2022-04-27 20:16:36 +0800

    feat: 添加全局 axios

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import store from './store'
 import '@/style/reset.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import request from './utils/request'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
+Vue.prototype.$request = request
 
 new Vue({
   router,
